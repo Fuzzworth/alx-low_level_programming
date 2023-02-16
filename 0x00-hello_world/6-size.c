f286a8da9079b88a5e86708afc0acd26a1d32a22
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 00:28:03 +0200

    Print sizes betty fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int integer = 1;
 long long_int = 1;
 double long_long_int = 1;
 float floater = 1.0;
-printf("Size of a %s: %d byte(s)\n","char",sizeof(character);
-printf("Size of an %s: %d byte(s)\n","int",sizeof(integer);
-printf("Size of a %s: %d byte(s)\n","long int",sizeof(long_int);
-printf("Size of a %s: %d byte(s)\n","long long int",sizeof(long_long_int);
-printf("Size of a %s: %d byte(s)\n","float",sizeof(floater);
+printf("Size of a %s: %d byte(s)\n", "char", sizeof(character);
+printf("Size of an %s: %d byte(s)\n", "int", sizeof(integer);
+printf("Size of a %s: %d byte(s)\n", "long int", sizeof(long_int);
+printf("Size of a %s: %d byte(s)\n", "long long int", sizeof(long_long_int);
+printf("Size of a %s: %d byte(s)\n", "float", sizeof(floater);
 return (0);
 }
