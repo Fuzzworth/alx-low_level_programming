8a637fa908f5ff58f039799b5e3bcf9d3e69c4ff
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 00:26:52 +0200

    Print sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char character = 'c';
+int integer = 1;
+long long_int = 1;
+double long_long_int = 1;
+float floater = 1.0;
+printf("Size of a %s: %d byte(s)\n","char",sizeof(character);
+printf("Size of an %s: %d byte(s)\n","int",sizeof(integer);
+printf("Size of a %s: %d byte(s)\n","long int",sizeof(long_int);
+printf("Size of a %s: %d byte(s)\n","long long int",sizeof(long_long_int);
+printf("Size of a %s: %d byte(s)\n","float",sizeof(floater);
+return (0);
+}
