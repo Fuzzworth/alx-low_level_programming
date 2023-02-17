165e5e05041c46bb5d8cc4f84490894f6707fe3e
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 15:01:14 +0200

    Last digit fix 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,8 @@
 * Description: Program generates a random number followed by
 * if the last digit of n is greater than 5: the string and is greater than 5
 * if the last digit of n is 0: the string and is 0
-* if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+* if the last digit of n is less than 6 and not 0: the string and is less than 
+* 6 and not 0
 *
 * Return: 0 if execution has no errors
 */
