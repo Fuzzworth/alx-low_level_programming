59b8d1eff9644fc73b2fea978cbbeead6f61f415
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 15:32:12 +0200

    Print Digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Program prints digits
+*
+* Description: Program prints all digits from 0 to 9
+*
+* Return: 0 if execution has no errors
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	pritnf("\n");
+	return (0);
+}
