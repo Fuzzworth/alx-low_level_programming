ea86daf6432fe6f0147b6b90dfcf2c744aa20685
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 15:11:03 +0200

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Program generates a random numbers
+*
+* Description: Program generates a random number
+* and prints it as well as its designation as positive,
+* negative or zero.
+*
+* Return: 0 if execution has no errors
+*/
+int main(void)
+{
+	for (int alphabet = 97; alphabet < 123; alphabet++)
+	{
+		printf("%d", alphabet);
+	}
+
+	printf("\n");
+	
+	return (0);
+}
