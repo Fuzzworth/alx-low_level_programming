dea4f42e304951e7f7fd7a3c53d7f3430c8739e6
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 14:40:56 +0200

    Negative or Positive betty fix 10

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,13 @@
 /* more headers goes there */
 #include <stdio.h>
 /**
-* main: - Program generates a random numbers
+* main - Program generates a random numbers
 *
 * Description: Program generates a random number
 * and prints it as well as its designation as positive,
 * negative or zero.
+*
+* Return: 0 if execution has no errors
 */
 int main(void)
 {
