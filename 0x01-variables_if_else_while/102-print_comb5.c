91fee8c17b2949dcc1a044b37e3426a398b66ebe
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 23:22:15 +0200

    Print all two two digit combinations fix 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 			{
 				for (digit_four  = 48; digit_four < 58; digit_four++)
 				{
-					if ((digit_one + digit_three) != (digit_two + digit_four))
+					if (((digit_one + digit_three) != (digit_two + digit_four)) || ((digit_two == digit_three) && (digit_one == digit_three)))
 					{
 						putchar(digit_one);
 						putchar(digit_two);
