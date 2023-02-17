a25a995e841e87afd3d2e054a7e43a2c23e50d74
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 15:18:46 +0200

    Print Alphabet fix 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,8 @@
 * main - Program prints the alphabet lowercase
 *
 * Description: Program prints lowercase characters of the aplphabet
-* followed by a new line
+* followed by a new line using only putchar and a for loop to
+* iterate over the ascii alphabet
 *
 * Return: 0 if execution has no errors
 */
