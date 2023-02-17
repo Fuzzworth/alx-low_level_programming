e928ff2651eebca94bade4a0a879d50a2e44e60e
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 15:25:06 +0200

    Print Alphabet lower than upper cases betty fix 1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 * main - Program prints the alphabet in lower then upper cases
 *
 * Description: Program prints lowercase characters of the aplphabet
-* followed by uppercase characters then finally a new line using only 
+* followed by uppercase characters then finally a new line using only
 * putchar and a for loop to iterate over the ascii alphabet
 *
 * Return: 0 if execution has no errors
