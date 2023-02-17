cbb22ffb2667080934d51167a71c446072aabc90
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-17 21:59:25 +0200

    Print all Two digit non repeating combos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,9 +19,13 @@ int main(void)
 			{
 				putchar(character_one);
 				putchar(character_two);
+			
+				if ( character_one != 56 & character_two != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			putchar(',');
-			putchar(' ');
 
 		}
 		
