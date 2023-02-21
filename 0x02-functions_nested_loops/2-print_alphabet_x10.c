097191809e6f495d8062cb53e287ca281fc44233
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-21 13:55:40 +0200

    Program that prints the alphabet lowercase, followed by a new line 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_alphabet - Program prints the lowercase alphabet 10 times
+*
+* Description: Program prints the lower case alphabet 10 times using
+*  the _putchar function and an nested for loop and the ascii table 
+*  character
+*
+*/
+void print_alphabet_x10(void)
+{
+	int ascii_character;
+	int iteration;
+
+	for (iteration = 1; iteration <= 10; iteration++)
+	{
+		for (ascii_character = 97; ascii_character < 123; ascii_character++)
+		{
+			_putchar(ascii_character);
+		}
+		_putchar('\n');
+	}
+}
