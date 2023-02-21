0b4a4fb3f322e8c848456a24c290321ede9869a4
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-21 17:14:48 +0200

    Function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* jack_bauer - Program prints the lowercase alphabet 10 times
+*
+* Description: Program prints the lower case alphabet 10 times using
+*  the _putchar function and an nested for loop and the ascii table
+*  character
+*
+*/
+void jack_bauer(void)
+{
+	int h_one;
+	int h_two;
+	int m_one;
+	int m_two;
+
+	for (h_one = 48; h_one <= 50; h_one++)
+	{
+		for (h_two = 48; h_two <= 51; h_two++)
+		{
+			for (m_one = 48; m_one <= 54; m_one++)
+			{
+				for (m_two = 48; m_two <= 57; m_two++)
+				{
+					_putchar(h_one);
+					_putchar(h_two);
+					_putchar(m_one);
+					_putcher(m_two);
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
