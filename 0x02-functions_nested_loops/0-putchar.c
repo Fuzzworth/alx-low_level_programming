c0a2671591a8f717ba855fc469c5bd69f4c40f8a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-21 12:19:52 +0200

    Program that prints _putchar, followed by a new line Betty FIX 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main - Program prints digits only using putchar and ascii tables
+* main - Program prints _putchar, followed by a new line.
 *
-* Description: Program prints digits using putchar and ascii tables
+* Description: Program prints _putchar, followed by a new line.
 *
 * Return: 0 if execution has no errors
 */
 int main(void)
 {
-	printf("_putchar");	
+	printf("_putchar\n");	
 	return (0);
 }
