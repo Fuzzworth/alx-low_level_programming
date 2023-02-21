278432c404ed2c688a4a92bd7d68550cae2f2bdc
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-21 16:20:40 +0200

    Function that prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_last_digit - Program prints the lowercase alphabet
+*
+* Description: Program prints the lower case alphabet using
+* _puthchar and a for loop over the ascii table character set
+*
+* @n: integer to print the last digit
+*
+* Return: returns the last digit
+*
+*/
+int print_last_digit(int n)
+{
+	return (n%10);
+}
