7367698b0f734b567104ec60116a7fd99112137a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-21 20:33:59 +0200

    Function that prints multiplication matrix

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+* print_times_table - Program prints the lowercase alphabet 10 times
+*
+* Description: Program prints the lower case alphabet 10 times using
+*  the _putchar function and an nested for loop and the ascii table
+*  character
+*
+*  @n: Integer between 0 and 15
+*
+*/
+void print_times_table(int n)
+{
+	int digit_one, digit_two, answer, last_digit, second_digit, third_digit;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (digit_one = 0; digit_one <= n; digit_one++)
+		{
+			for (digit_two = 0; digit_two <= n; digit_two++)
+			{
+				answer = digit_one * digit_two;
+				last_digit = answer % 10;
+				second_digit = answer / 10;
+				third_digit = answer / 100;
+				if (digit_two == 0)
+				{
+					_putchar(answer + 48);
+					_putchar(44);
+					continue;
+				}
+				_putchar(' ');
+				if (third_digit > 0)
+					_putchar(third_digit + 48);
+				else
+					_putchar(' ');
+				if (second_digit > 0)
+					_putchar(second_digit + 48);
+				else
+					_putchar(' ');
+				_putchar(last_digit + 48);
+				if (digit_two != 9)
+					_putchar(44);
+			}
+			_putchar('\n');
+		}
+	}
+}
