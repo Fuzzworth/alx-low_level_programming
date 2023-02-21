bc970b2c1f29579913a9107ace9832812bac39ff
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-22 01:43:16 +0200

    Function that prints all natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Program prints digits only using putchar and ascii tables
+*
+* Description: Program prints digits using putchar and ascii tables
+*
+* Return: 0 if execution has no errors
+*/
+int main(void)
+{
+	int sum;
+
+	sum = 0;
+
+	for (int i = 0; i < 1024; ie++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n",sum);
+	return (0);
+}
