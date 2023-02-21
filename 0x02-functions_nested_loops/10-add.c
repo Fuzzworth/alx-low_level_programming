1fc08fabb8a6ad99a0efc3793926afcb9f25e15b
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-21 18:21:48 +0200

    Function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* add - Program prints the lowercase alphabet
+*
+* Description: Program prints the lower case alphabet using
+* _puthchar and a for loop over the ascii table character set
+*
+* @a: ASCII equivalent integer
+* @b: Integer to add
+*
+* Return: the addition of a and b
+*
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
