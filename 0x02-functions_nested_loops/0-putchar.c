2eb228535f37415d82a1cca80d7e06d84896ae04
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-21 12:18:48 +0200

    Program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Program prints digits only using putchar and ascii tables
+*
+* Description: Program prints digits using putchar and ascii tables
+*
+* Return: 0 if execution has no errors
+*/
+int main(void)
+{
+	printf("_putchar");	
+	return (0);
+}
