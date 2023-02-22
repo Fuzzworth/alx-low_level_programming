4df4b7acabbb9f0168b7ca6ef9d19a42396f8f0b
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-22 03:25:57 +0200

    Function that prints fibonacci numbers sum of even

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,9 @@ int main(void)
 {
 	int i;
 	unsigned long t1, t2, nextTerm;
-
+	unsigned long t1_h1, t1_h2, t2_h1, t2_h2;
+	unsigned long h1, h2;
+	
 	t1 = 1;
 	t2 = 2;
 	nextTerm = t1 + t2;
@@ -24,6 +26,29 @@ int main(void)
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
 	}
+
+	t1_h1 = t1 / 10000000000;
+	t2_h1 = t2 / 10000000000;
+	t1_h2 = t1 % 10000000000;
+	t2_h2 = t2 % 10000000000;
+
+	for (i = 93; i <= 98; i++)
+	{
+		h1 = t1_h1 + t2_h1;
+		h2 = t1_h2 + t2_h2;
+		if ((t1_h2 + t2_h2) > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+		printf(", %lu%lu", h1, h2);
+
+		t1_h1 = t2_h1;
+		t1_h2 = t2_h2;
+		t2_h1 = h1;
+		t2_h2 = h2;
+	}
+
 	printf("\n");
 	return (0);
 }
