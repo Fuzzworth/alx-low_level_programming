3f0e25a7e1afc171a6cd4d4c4a56d63a4d4fe788
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-22 02:26:11 +0200

    Function that prints fibonacci numbers sum of even

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Program prints digits only using putchar and ascii tables
+*
+* Description: Program prints digits using putchar and ascii tables
+*
+* Return: 0 if execution has no errors
+*/
+int main(void)
+{
+	int i;
+	unsigned long t1, t2, nextTerm;
+
+	t1 = 1;
+	t2 = 2;
+	nextTerm = t1 + t2;
+
+	printf("%lu, %lu", t1, t2);
+
+	for (i = 3; i <= 98; ++i)
+	{
+		printf(", %lu", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	printf("\n");
+	return (0);
+}
