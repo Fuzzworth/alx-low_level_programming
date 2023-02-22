0144087001a053fa14c086875d229423fd0ef1c2
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-22 02:01:34 +0200

    Function that prints fibonacci numbers less ones that start with 1 or 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Program prints digits only using putchar and ascii tables
+*
+* Description: Program prints digits using putchar and ascii tables
+*
+* Return: 0 if execution has no errors
+*/
+int main(void)
+{
+	int t1, t2, nextTerm;
+
+	t1 = 0;
+	t2 = 1;
+	nextTerm = t1 + t2;
+
+	printf("%d, %d, ", t1, t2);
+
+	for (i = 3; i <= 50; ++i) {
+		printf("%d, ", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	return 0;
+}
