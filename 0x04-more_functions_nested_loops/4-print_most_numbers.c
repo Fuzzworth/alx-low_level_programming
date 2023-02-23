4ea7347e205b2e8c2cef546e7142325d2f4ba5b7
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-23 16:45:08 +0200

    Function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints odd numbers, from 0 to 9
+ *
+ * Description: function that prints the odd numbers
+ * from 0 to 9, followed by a new line.
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number % 2 == 0)
+			_putchar(number);
+	}
+	_putchar('\n');
+}
