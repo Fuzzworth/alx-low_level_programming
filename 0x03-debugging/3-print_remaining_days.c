1783a1716105cbc34a7bc5a4e49f6ef5e22250b8
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-23 15:04:35 +0200

    commit 8 debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,10 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
-		day++;	
+		if (day >= 60)
+		{
+			day++;
+		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
