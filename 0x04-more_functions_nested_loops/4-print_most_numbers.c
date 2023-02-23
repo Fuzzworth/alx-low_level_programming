530562a953af67fa38abd62f7a8ffbadfd2362d6
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-23 16:47:52 +0200

    Function that prints the numbers, from 0 to 9, followed by a new line FIX: 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -15,7 +15,7 @@ void print_most_numbers(void)
 
 	for (number = '0'; number <= '9'; number++)
 	{
-		if (number == '0' || number % 2 == 1)
+		if (number != '2' || number != '4')
 			_putchar(number);
 	}
 	_putchar('\n');
