01dbbe371c6e0cd594802e3ef42abb846fa19835
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-24 02:18:27 +0200

    Function that prints an integer FIX

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,25 +14,25 @@ void print_number(int n)
 {
 	if (n < 10 && n > 0)
 	{
-		_putchar(n + '0');
+		_putchar(n + 48);
 	}
 	else if (n > -10 && n < 0)
 	{
-		_putchar('-');
-		_putchar(-n + '0');
+		_putchar(45);
+		_putchar(-n + 48);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 	}
 	else if (n < 0)
 	{
 		print_number(n / 10);
-		_putchar(((-n) % 10) + '0');
+		_putchar(((-n) % 10) + 48);
 	}
 	else if (n > 0)
 	{
 		print_number(n / 10);
-		_putchar((n % 10) + '0');
+		_putchar((n % 10) + 48);
 	}
 }
