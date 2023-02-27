e0f40c848f71af117388cc3a76b577529e6a7203
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-02-27 18:39:15 +0200

    Program that generates random valid passwords for the program 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Password generator
+ *
+ * Description: program that generates random valid passwords
+ * for the program 101-crackme
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	printf("%d\n", srand());
+	return (0);
+}
