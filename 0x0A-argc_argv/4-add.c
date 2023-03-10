44b86ef7e66598ae4a1a18ce992d6cf3309a7af7
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-10 18:46:08 +0200

    Adds numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int isdigit(int n)
+{
+	return (n >= '0' && n <= '9' || n == '-' || n == '+');
+}
+
+int isNumber(char *s)
+{
+	int i, isD;
+
+	isD = 0;
+
+	for (int i = 0; s[i]!= '\0'; i++)
+	{
+		isD = isdigit(s[i]);
+		if (!isD)
+			return 0;
+	}
+	return 1;
+}
+
+/**
+ * main - prints its name, followed by a new line
+ *
+ * Description: prints its name, followed by a new line
+ *
+ * @argc: number of arguments
+ * @argv: Command line arguments
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, index;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		sum = 0;
+	}
+	else
+	{
+		for (index = 1; index < argc; index++)
+		{
+			if (isNumber(argv[index]))
+			{
+				sum += atoi(argv[index]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n",sum);
+	return (0);
+}
