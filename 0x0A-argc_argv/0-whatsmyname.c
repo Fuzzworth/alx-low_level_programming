dcf354ca9d32795f017bb0cb58495cbc7ecf611a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-10 17:27:20 +0200

    Prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ *
+ * Description: prints its name, followed by a new line
+ *
+ * @argc: number of arguments
+ * @argv: Command line arguments
+ *
+ * Return: 0;
+ */
+
+int main (char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
