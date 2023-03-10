6a0a98381bac31656e16375a53de1e3de8926394
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-10 18:05:38 +0200

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ *
+ * Description: prints its name, followed by a new line
+ *
+ * @argc: number of arguments
+ * @argv: Command line arguments
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	printf("%d\n", ((int) argv[1]) * ((int) argv[2]));
+
+	return (0);
+}
