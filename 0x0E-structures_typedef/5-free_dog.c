4768c1b39db779c4567753262bfd8c686f1791ff
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-20 21:57:42 +0200

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+
+/**
+ * free_dog - function that frees a struct dog
+ *
+ * Description: function that frees a struct dog
+ *
+ * @d: Struct of dog type
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		free(d);
+	}
+}
