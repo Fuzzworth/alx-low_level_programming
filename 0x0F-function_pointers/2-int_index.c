4ce56e565db9d688c20364c67f179df345cd8d00
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-23 01:54:18 +0200

    Function that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - short description
+ *
+ * Description: long description
+ *
+ * @array: argument_1 description
+ * @size: argument_2 description
+ * @action: argument_3 description
+ *
+ * Return: void
+ */
+
+void int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array != 0 && size > 0  && cmp != 0)
+		for (index = 0; index < size; index++)
+			if(cmp(array[index]))
+				return (index);
+	return (-1);
+}
