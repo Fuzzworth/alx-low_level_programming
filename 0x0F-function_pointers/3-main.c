c9029ea46d6a3e2cb0993f7d7e1ca96339da4bac
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-23 03:46:01 +0200

    Created 3-main.c:

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,7 +15,6 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
 	int (*op)(int, int);
 
 	if (argc != 4)
@@ -23,14 +22,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 	op = get_op_func(argv[2]);
 	if (op == 0)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", op(a, b));
+	printf("%d\n", op(atoi(argv[1]);, atoi(argv[3]);
+));
 	return (0);
 }
