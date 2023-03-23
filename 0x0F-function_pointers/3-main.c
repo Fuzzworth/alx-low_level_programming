36ac86143c6083513210b783f0dceddc047903c8
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-23 02:42:57 +0200

    Created 3-main.c:

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name, followed by a new line
+ *
+ * Description: prints its name, followed by a new line
+ *
+ * @argc: number of arguments
+ * @argv: Command line arguments
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op = get_op_func(argv[2]);
+	result = op(a,b);
+
+	return (0);
+}
