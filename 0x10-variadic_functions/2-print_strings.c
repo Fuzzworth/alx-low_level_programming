c465a3e28bd881c1c593f59a662bc82c82faebc2
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-23 13:15:42 +0200

    Function that prints strings, followed by a new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_srings - function that prints strings, followed by a new line.
+ *
+ * Description: function that prints strings, followed by a new line.
+ *
+ * @n: indefinite number of integer arguments
+ * @separator: string seperator
+ *
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list all_parameters;
+	unsigned int index;
+	char *temp_string;
+
+	va_start(all_parameters, n);
+
+	for (index = 0; index < n; index++)
+	{
+		temp_string = (char *) va_arg(all_parameters, int);
+		printf("%s", temp_string ? temp_string : "(nil)");
+		if (index != (n - 1) && separator != 0)
+			printf("%s", separator);
+	}
+	va_end(all_parameters);
+	printf("\n");
+}
