7e5e6ddc7760cd9b43230e07ec9e3c7285fe92e5
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-23 02:06:17 +0200

    This file should contain the 5 following functions (not more)

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+/**
+ * op_add - short description
+ *
+ * Description: long description
+ *
+ * @a: argument_1 description
+ * @b: argument_2 description
+ *
+ * Return: return description
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - short description
+ *
+ * Description: long description
+ *
+ * @a: argument_1 description
+ * @b: argument_2 description
+ *
+ * Return: return description
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - short description
+ *
+ * Description: long description
+ *
+ * @a: argument_1 description
+ * @b: argument_2 description
+ *
+ * Return: return description
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - short description
+ *
+ * Description: long description
+ *
+ * @a: argument_1 description
+ * @b: argument_2 description
+ *
+ * Return: return description
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - short description
+ *
+ * Description: long description
+ *
+ * @a: argument_1 description
+ * @b: argument_2 description
+ *
+ * Return: return description
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
