4bc5b2adb388c5aee6c335c3767be973fd701224
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-23 03:51:35 +0200

    function that selects the correct function to perform the operation asked by the user. You’re not allowed to declare any other function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -25,7 +27,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (s != 0 && ops[i].op[0] != 0 && s[0] == ops[i].op[0])
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
