c80a490675f1593dd1017f2352ff8dfbc88f63f7
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-31 01:39:51 +0200

    Function that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * function_name - short description
+ *
+ * Description: long description
+ *
+ * @argument_1: argument_1 description
+ * @argument_2: argument_2 description
+ *
+ * Return: return description
+ */
+
+size_t print_list(const list_t *h)
+{
+	// return (0);
+}
