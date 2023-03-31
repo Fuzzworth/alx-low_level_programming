4439d1122397b6dc3a8878c9a85b544fe4901289
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-31 03:04:24 +0200

    Function that prints You're beat! and yet, you must allow,I bore my house upon my backbefore the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * before_main - short description
+ *
+ * Description: long description
+ *
+ * Return: return description
+ */
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
