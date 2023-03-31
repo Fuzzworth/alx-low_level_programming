dc8d4aa290c446bb3c4fe1b3664b25f7338c3dfe
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-31 03:27:50 +0200

    64bit assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-; ----------------------------------------------------------------------------------------
-; Writes "Hello, Holberton" to the console using a C library. Runs on Linux.
-;
-;     nasm -f elf64 101-hello_holberton.asm && gcc -no-pie -std=gnu89 101-hello_holberton.o -o hello
-; ----------------------------------------------------------------------------------------
-
-          global    main
-          extern    printf
-
-          section   .text
-main:                                       ; This is called by the C library startup code
-          mov       rdi, message            ; First integer (or pointer) argument in rdi
-          call      printf                    ; puts(message)
-          ret                               ; Return from main back into C library wrapper
-message:
-          db        "Hello, Holberton", 0        ; Note strings must be terminated with 0 in C
+	global    main
+	extern    printf
+main:
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov 	eax, 0
+	ret
+format: db `Hello, Holberton\n`,0
