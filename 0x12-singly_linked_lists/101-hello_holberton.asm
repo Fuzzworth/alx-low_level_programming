c2890b1d8d8cbc7afa47975b4a852347eda6c458
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-31 03:15:21 +0200

    64bit assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,21 @@
-section .data
+; ----------------------------------------------------------------------------------------
+; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
+; To assemble and run:
+;
+;     nasm -felf64 hello.asm && ld hello.o && ./a.out
+; ----------------------------------------------------------------------------------------
 
-    message db "Hello, Holberton\n", 10
+          global    _start
 
-section .text
+          section   .text
+_start:   mov       rax, 1                  ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 13                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 60                 ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
 
-global _start
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 14
-    syscall
+          section   .data
+message:  db        "Hello, Holberton\n", 10      ; note the newline at the end
