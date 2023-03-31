ff10530209e432052455d28b2193e3a7033ad349
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-31 03:25:06 +0200

    64bit assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,12 +5,12 @@
 ; ----------------------------------------------------------------------------------------
 
           global    main
-          extern    puts
+          extern    printf
 
           section   .text
 main:                                       ; This is called by the C library startup code
           mov       rdi, message            ; First integer (or pointer) argument in rdi
-          call      puts                    ; puts(message)
+          call      printf                    ; puts(message)
           ret                               ; Return from main back into C library wrapper
 message:
           db        "Hello, Holberton", 0        ; Note strings must be terminated with 0 in C
