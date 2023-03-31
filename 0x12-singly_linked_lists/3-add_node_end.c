161c87b0ae65c8be1ca9bb94043d975f47a500f0
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-31 02:11:32 +0200

    Function that adds a new node at the end of a list_t list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -21,6 +21,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	while (currnet_node->next != 0)
 	{
+		printf("[%d] %s", currnet_node->len, currnet_node->str);
 		currnet_node = currnet_node->next;
 	}
 
