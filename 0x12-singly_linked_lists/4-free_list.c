8cee4a5afe663144eb98b381828cd92a53f745f8
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-03-31 02:52:17 +0200

    Function that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - short description
+ *
+ * Description: long description
+ *
+ * @head: argument_1 description
+ *
+ * Return: return description
+ */
+
+void free_list(list_t *head)
+{
+	while(head)
+	{
+		free(head->str);
+	}
+}
