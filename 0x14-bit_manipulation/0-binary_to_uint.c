729be75acd7841e2979eedcb3d08cc0e11a71b1c
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-04-10 21:38:48 +0200

    Function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,45 @@
 #include "main.h"
 
+/**
+ * string_length - function retruns the length of a string
+ *
+ * Description: function returns the length of a string
+ *
+ * @b: Pointer to a string of 0 and 1 chars
+ *
+ * Return: size of string
+ */
 int string_length(const char *b)
 {
 	int index, string_length;
 
 	string_length = 0;
+	if (b == 0)
+		return (string_length);
 	for (index = 0; b[index]; index++)
 		string_length++;
 	return (string_length);
 }
 
+/**
+ * power - function that returns the exponential power
+ *
+ * Description: function that returns the exponential power
+ *
+ * @base: base integer
+ * @exponent: exponential integer
+ *
+ * Return: unsigned integer
+ */
 unsigned int power(int base, int exponent)
 {
 	unsigned int total;
 
 	total = 1;
+	if (base < 0)
+		return (0);
+	if (exponent == 0)
+		return (total);
 	while (exponent > 0)
 	{
 		total *= base;
@@ -32,7 +57,6 @@ unsigned int power(int base, int exponent)
  *
  * Return: unsigned number or 0 is not valid
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	int binary_string_position, binary_string_length;
