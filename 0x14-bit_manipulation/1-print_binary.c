3c5e3b4b79ec31dd105c4b53f290d8ba2c73157d
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-04-10 21:56:28 +0200

    Function that prints the binary representation of a number.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,30 +13,22 @@
 void print_binary(unsigned long int n)
 {
 	int number_of_digits_printed, sign, index;
-	unsigned int num2;
 	char num_rev_string[15];
 
 	number_of_digits_printed = sign = 0;
 
-	if (n < 0)
-	{
-		sign = 1;
-		num2 = -(n);
-	}
-	else
-		num2 = n;
 
 	while (1)
 	{
-		if ((num2 / 10) > 0)
+		if ((n / 10) > 0)
 		{
-			num_rev_string[number_of_digits_printed] = ((num2 % 10) + '0');
+			num_rev_string[number_of_digits_printed] = ((n % 10) + '0');
 			number_of_digits_printed++;
-			num2 /= 10;
+			n /= 10;
 		}
-		else if ((num2 / 10) < 10)
+		else if ((n / 10) < 10)
 		{
-			num_rev_string[number_of_digits_printed] = ((num2 % 10) + '0');
+			num_rev_string[number_of_digits_printed] = ((n % 10) + '0');
 			number_of_digits_printed++;
 			if (sign)
 			{
