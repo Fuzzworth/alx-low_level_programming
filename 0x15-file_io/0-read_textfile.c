cb036eca93171143a211d4c3b4a5a85d9a97f985
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-04-11 01:53:56 +0200

    Function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -33,12 +33,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	character_to_print = fgetc(fp);
 	while (number_of_characters_printed <= letters)
 	{
-		printf("%c", character_to_print);
 		if (number_of_characters_printed == letters || character_to_print == -1)
 		{
 			fclose(fp);
 			return (number_of_characters_printed);
 		}
+		printf("%c", character_to_print);
 		number_of_characters_printed++;
 		character_to_print = fgetc(fp);
 	}
