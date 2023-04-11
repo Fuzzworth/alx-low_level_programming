4601e3682501f2baa209ddbd5540af482c6865f2
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-04-11 02:10:49 +0200

    Function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,14 @@ int create_file(const char *filename, char *text_content)
 {
 	FILE *ptr;
 
+	if (filename == NULL || text_content == NULL)
+		return (-1);
 	ptr = fopen(filename, "w");
 	if (ptr == NULL)
 	{
 		return (-1);
 	}
+	fputs(text_content, ptr);
 	fclose(ptr);
 	return (1);
 }
