0e8e9c8565006b60d3808d1174a4f7ed4433bf81
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-04-11 02:08:08 +0200

    Function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * create_file - short description
+ *
+ * Description: long description
+ *
+ * @filename: argument_1 description
+ * @text_content: argument_2 description
+ *
+ * Return: return description
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE* ptr;
+	
+	ptr = fopen(filename, "w");
+	if (ptr == NULL)
+	{
+		return (-1);
+	}
+	fclose(ptr);
+	return (1);
+}
