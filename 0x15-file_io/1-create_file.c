ddf5870fdccb4f39aad4c551d07f505485a56bca
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-04-11 02:39:17 +0200

    F

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 		text_content = "";
-	
+
 	if (write(ptr, text_content, strlen(text_content) == -1))
 			return (-1);
 	close(ptr);
