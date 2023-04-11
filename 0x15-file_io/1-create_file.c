505167102b20f4b9a2c4331c4f8c63ad5f9411e4
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-04-11 02:34:01 +0200

    F

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
  * create_file - short description
@@ -19,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	ptr = fopen(filename, "w");
+	ptr = fopen(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (ptr == NULL)
 	{
 		return (-1);
