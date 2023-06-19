fd72d0301d5c808b78d852dd56db19533208d335
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-19 15:37:46 +0200

    Task 4 pick_numbers shared object Try 14

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/Fuzzworth/alx-low_level_programming/master/0x18-dynamic_libraries/librand.so librand.so
-export LD_PRELOAD=$PWD/librand.so
+export LD_PRELOAD="./librand.so"
