22ded0d3192652a891862314865a0ae327770579
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-19 23:13:41 +0200

    Found it

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/Fuzzworth/alx-low_level_programming/master/0x18-dynamic_libraries/librand.so librand.so
-export LD_PRELOAD="./librand.so"
+wget https://raw.githubusercontent.com/Fuzzworth/alx-low_level_programming/master/0x18-dynamic_libraries/librand.so /home/librand.so
+LD_PRELOAD="/home/librand.so"
