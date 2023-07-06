724689373562d5e9496ed0d1f37985a5b5235bbc
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-07 01:56:06 +0200

    task 3 FIX: 1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *new_node = NULL, *old_head = NULL;
 
-	if (key == NULL)
+	if (key == NULL || value == NULL || ht == NULL)
 		return (0);
 	if (strlen(key) == 0)
 		return (0);
